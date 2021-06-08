118ed4e9513f3ad01ccb74c4f7c0830f485d433f
Simchaja <53301183+Simchaja@users.noreply.github.com>
2021-06-08 23:21:54 +0200

    Start JSON Parsing

--- app/src/main/java/INF1D/eclipse/setup/fragments/loginFragment.java
+++ app/src/main/java/INF1D/eclipse/setup/fragments/loginFragment.java
@@ -1,6 +1,7 @@
 package INF1D.eclipse.setup.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,8 +22,10 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 import INF1D.eclipse.R;
@@ -45,21 +48,46 @@ public class loginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        requireView().findViewById(R.id.nextButton).setOnClickListener(v -> ((SetupActivity) Objects.requireNonNull(getActivity())).nextButton(view));
-        /*
+        requireView().findViewById(R.id.nextButton).setOnClickListener(v -> ((SetupActivity) requireActivity()).nextButton(view));
+
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String url ="http://eclipse.serverict.nl/api/widgets";
+                String url ="http://eclipse.serverict.nl/api/mirrors";
+                url ="https://api.androidhive.info/contacts/";
                 RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
 
                 //create Json request
-                JsonObjectRequest jsonArrayRequest = new JsonObjectRequest (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        binding.response.setText(response.toString());
+                        try {
+                            binding.response.setText(response.toString());
+                            JSONArray contacts = response.getJSONArray("contacts");
+                            String jsonResponse = "names equals: ";
+                            for (int i = 0; i < contacts.length(); i++) {
+                                JSONObject c = contacts.getJSONObject(i);
+
+                                String id = c.getString("id");
+                                String name = c.getString("name");
+                                String email = c.getString("email");
+                                String address = c.getString("address");
+                                String gender = c.getString("gender");
+
+                                // Phone node is JSON Object
+                                JSONObject phone = c.getJSONObject("phone");
+                                String mobile = phone.getString("mobile");
+                                String home = phone.getString("home");
+                                String office = phone.getString("office");
+
+                                jsonResponse += name + " ";
+                            }
+                            binding.response.setText(jsonResponse);
+                        } catch (final JSONException e) {
+                            binding.response.setText(e.getMessage());
+                        }
                     }
                 }, new Response.ErrorListener() {
 
@@ -70,9 +98,9 @@ public class loginFragment extends Fragment {
                     }
                 });
                 // Access the RequestQueue through your singleton class.
-                queue.add(jsonArrayRequest);
+                queue.add(jsonObjectRequest);
             }
-        });*/
+        });
 
     }
 
